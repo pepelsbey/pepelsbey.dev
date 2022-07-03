479cd752b119b0a71570d545cd0931ef7acbcfcc
Vadim Makeev <hi@pepelsbey.dev>
2022-07-03 18:32:26 +0200

    Add some more links

--- src/data/global.js
+++ src/data/global.js
@@ -2,6 +2,7 @@ module.exports = {
     'title': 'Vadim Makeev',
     'description': 'DESC',
     'domain': 'https://pepelsbey.dev',
+    'email': 'hi@pepelsbey.dev',
     'language': 'en',
     'copyright': 'CC BY-NC-ND 4.0',
     'year': new Date().getFullYear(),
