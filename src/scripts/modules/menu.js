829079789af50ff961530df9e07237256daea407
Vadim Makeev <hi@pepelsbey.dev>
2022-07-31 01:10:22 +0200

    Refactor popup menu

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -1,11 +1,23 @@
+import { createFocusTrap } from 'focus-trap';
+// import wicg-inert from 'wicg-inert';
+
+const header = document.querySelector('.header');
 const menu = document.querySelector('.menu');
 const headerButton = document.querySelector('.header__button');
-const menuButton = document.querySelector('.menu__button');
+const headerLink = document.querySelector('.header__link');
+const focusTrap = createFocusTrap(header);
 
-headerButton.addEventListener('click', (event) => {
-    menu.removeAttribute('hidden');
-});
+headerButton.addEventListener('click', () => {
+    const isExpanded = headerButton.getAttribute('aria-expanded') === 'true';
 
-menuButton.addEventListener('click', (event) => {
-    menu.setAttribute('hidden', '');
+    headerButton.setAttribute('aria-expanded', isExpanded ? 'false' : 'true');
+    headerLink.setAttribute('tabindex', isExpanded ? '0' : '-1');
+
+    menu.toggleAttribute('hidden');
+
+    if (isExpanded) {
+        focusTrap.deactivate();
+    } else {
+        focusTrap.activate();
+    }
 });
