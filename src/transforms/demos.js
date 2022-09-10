c72b240910509b5be0e04c876fa3b0788d83ffd9
Vadim Makeev <hi@pepelsbey.dev>
2022-09-10 23:15:50 +0200

    Remove duplicated

--- src/transforms/demos.js
+++ src/transforms/demos.js
@@ -26,26 +26,4 @@ module.exports = function(window) {
 
         iframe.replaceWith(wrapper);
     }
-
-    [...iframes]
-        .forEach((iframe) => {
-            const source = iframe.getAttribute('src');
-            const wrapper = window.document.createElement('figure');
-
-            wrapper.classList.add('figure');
-            iframe.classList.add('figure__content');
-
-            wrapper.innerHTML = `
-                ${iframe.outerHTML}
-                <figcaption class="figure__caption">
-                    <a class="figure__link action" href="${source}" target="_blank" rel="nofollow noopener noreferrer">
-                        Open in the new tab
-                        <svg class="action__icon" width="24" height="24" aria-hidden="true">
-                            <use href="/images/icons.svg#external">
-                        </svg>
-                    </a>
-                </figcaption>
-            `;
-            iframe.replaceWith(wrapper);
-        });
 }
