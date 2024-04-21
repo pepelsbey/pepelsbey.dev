a48fccdbc32c7f93f6fc619e8e9d60845e183fa0
Nikita Gusakov <gusnkt@gmail.com>
2024-04-21 13:23:22 +0200

    Improve title copying fix

--- src/scripts/modules/lead-copy.js
+++ src/scripts/modules/lead-copy.js
@@ -1,7 +1,15 @@
+function getSelectionTextContent() {
+	const selection = document.getSelection();
+	const range = selection?.getRangeAt(0);
+	const clone = range?.cloneContents();
+	return clone?.textContent;
+}
+
 document.querySelector('.lead__title')?.addEventListener('copy', (event) => {
+	const selectionText = getSelectionTextContent();
 	event.clipboardData.setData(
 		'text/plain',
-		event.target.textContent
+		selectionText ?? event.target.textContent
 	);
 
 	event.preventDefault();
